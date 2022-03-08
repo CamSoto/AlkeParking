3e88198e3246b5dbb044163fe025b548ff6cca8e
Leandro Manuel Busquets <lbusquets@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2022-03-08 17:41:37 -0300

    add point 5

--- src/main/kotlin/Parking.kt
+++ src/main/kotlin/Parking.kt
@@ -1,5 +1,16 @@
 /*por que usamos un set? porque si.
 * y porque no tiene repetidos , los objetos?? son unicos*/
 data class Parking(val vehicles: MutableSet<Vehicle>) {
+    val maxVehicle = 20
 
+    fun addVehicle(vehicle: Vehicle) : Boolean{
+        return if (maxVehicle > vehicles.count()) {
+            vehicles.add(vehicle)
+            true
+        }
+        else {
+            false
+        }
+
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,15 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val car = Vehicle("AA111AA", VehicleType.CAR, discountCard = "DISCOUNT_CARD_001")
+    val motorcycle = Vehicle("B222BBB", VehicleType.MOTORCYCLE)
+    val minibus = Vehicle("CC333CC", VehicleType.MINIBUS)
+    val bus = Vehicle("DD444DD", VehicleType.BUS, discountCard = "DISCOUNT_CARD_002")
+    val parking = Parking(mutableSetOf(car, motorcycle, minibus, bus))
 
+    println(parking.vehicles.contains(car))
+    println(parking.vehicles.contains(motorcycle))
+    println(parking.vehicles.contains(minibus))
+    println(parking.vehicles.contains(bus))
+    parking.vehicles.remove(motorcycle)
+    println(parking.vehicles.size)
 }
 
